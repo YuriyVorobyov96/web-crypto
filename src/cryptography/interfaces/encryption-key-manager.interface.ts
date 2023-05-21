aaa57eee641fb437f232d228e350adaefd69c5c4
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-05-21 22:45:46 +0300

    feature/crypto: service implements key operations

--- src/cryptography/interfaces/encryption-key-manager.interface.ts
+++ src/cryptography/interfaces/encryption-key-manager.interface.ts
@@ -1,4 +1,5 @@
 export default interface IEncryptionKeyManager {
   readonly parseKey: (key: ArrayBuffer) => Promise<CryptoKey>;
   readonly readKey: (key: CryptoKey) => Promise<ArrayBuffer>;
+  readonly generateKey: (key: CryptoKey) => Promise<CryptoKey>;
 }
